55f3df1aff878944c8e772187257112912e3aba6
desarrollo.externo@hsg <desarrollo.externo@hsg>
2025-11-19 15:14:36 -0300

    Feat(master): Inicio proyecto

---
+++ src/main/java/com/kimcura/kimcura_colegios/KimcuraColegiosApplication.java
@@ -0,0 +1,13 @@
+package com.kimcura.kimcura_colegios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimcuraColegiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimcuraColegiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimcura/kimcura_colegios/KimcuraColegiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimcura.kimcura_colegios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimcuraColegiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
