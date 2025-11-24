9f00a978445a370e514c829e164e006de8bda6cf
Nathan <nathan@MacBook-Pro-de-Nathan.local>
2025-11-24 20:09:34 -0300

    Feat(master): Refactoring

--- src/main/java/com/kimcura/kimcura_colegios/controller/ColegioController.java
+++ src/main/java/com/kimcura/kimcura_colegios/controller/ColegioController.java
@@ -5,7 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kimcura.kimcura_colegios.model.ColegioModel;
 import com.kimcura.kimcura_colegios.service.ColegioService;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @RestController
 @RequestMapping("/colegios")
@@ -22,5 +32,24 @@ public class ColegioController {
         return colegioService.getAllColegios();
     }
     
+    @GetMapping("{id}")
+    public Optional<ColegioModel> getColegioById(@RequestParam UUID id) {
+        return colegioService.getColegioById(id);
+    }
+    
+    @PutMapping
+    public ColegioModel setColecio(@RequestBody ColegioModel colegio) {
+        return colegioService.createColegio(colegio);
+    }
+
+    @PutMapping("{id}")
+    public ColegioModel updateColegioById(@PathVariable UUID id, @RequestBody ColegioModel colegio) {
+        return colegioService.updateColegioById(id, colegio);
+    }
+
+    @DeleteMapping("{id}")
+    public Boolean deleteColegioById(@PathVariable UUID id) {
+        return colegioService.removeColegioById(id);    
+    }
 
 }
