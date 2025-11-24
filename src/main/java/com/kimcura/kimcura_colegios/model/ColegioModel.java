5de025ec51e6d64b59e2129c82f67220e48119da
Jonathan Bernal Veras_Externo <jbernal.externo@cmfchile.cl>
2025-11-24 15:20:45 -0300

    Feat(master): Creando API

---
+++ src/main/java/com/kimcura/kimcura_colegios/model/ColegioModel.java
@@ -0,0 +1,102 @@
+package com.kimcura.kimcura_colegios.model;
+
+import java.sql.Date;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "colegios")
+public class ColegioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+    @Column(name = "azure_entra_id")
+    private String azureEntraId;
+    @Column(name = "school_id")
+    private UUID schoolId;
+    @Column(name = "role")
+    private String role;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "is_active")
+    private Boolean isActive;
+    @Column(name = "created_at")
+    private Date createdAt;
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    public String getAzureEntraId() {
+        return azureEntraId;
+    }
+    public void setAzureEntraId(String azureEntraId) {
+        this.azureEntraId = azureEntraId;
+    }
+    public UUID getSchoolId() {
+        return schoolId;
+    }
+    public void setSchoolId(UUID schoolId) {
+        this.schoolId = schoolId;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public Boolean getIsActive() {
+        return isActive;
+    }
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    
+}
